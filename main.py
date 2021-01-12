c5e144a36c3bc48a3fefda55de5bccd1b958e413
Luuk Weiss <luuk.weiss@takeaway.com>
2021-01-12 18:20:19 +0100

    updated switch case to just a if else and added loop to keep program running

--- main.py
+++ main.py
@@ -8,31 +8,29 @@ def user_input_switch():
     txt_input = input("Welcome to this ASN_BANK application!\n"
                       "Option 1. Enter a csv file_name to scan\n"
                       "Option 2. Create a new group with search queries\n"
-                      "Option 3. Print all search queries\n"
+                      "Option 3. Print all search queries\n"    # Still in progress
                       "Option 4. Print this message again\n"
                       "Enter a number: ")
-    input_switch(txt_input)
-
-
-def set_csv_file_location():
-    return readingcsv.ask_csv_file()
-
-
-def exit_program():
-    return print("default, exiting program..")
+    return input_switch(txt_input)
 
 
 def input_switch(input_arg):
-    program_functions = {
-        1: set_csv_file_location,
-        3: plotting.plot_histo,
-        4: user_input_switch,
-        9: exit_program
-    }
-    func = program_functions.get(input_arg, lambda: "exiting program..")
-    print(func())
+    if input_arg == "1":
+        readingcsv.ask_csv_file()
+        return False
+    elif input_arg == "2":
+        plotting.plot_histo(readingcsv.global_expense_value_dictionary)
+        return False
+    elif input_arg == "4":
+        user_input_switch()
+        return False
+    else:
+        print("default, exiting program..")
+        return True
 
 
 if __name__ == '__main__':
-    user_input_switch()
+    while True:
+        if user_input_switch():
+            break
     # file names: resources/0708742548_28122020_215932.csv
